eccb0e86c5cc64bcd2673e8a06cf49c4d0b158cf
Q∀Z <n3@nitbit.dev>
2024-10-27 08:23:38 +0800

    "jason"

--- app/(posts)/page.tsx
+++ app/(posts)/page.tsx
@@ -15,6 +15,8 @@ const Home: React.FC = () => {
   const [isExpandPosts, setIsExpandPosts] = useState(false);
   const [upperHomeHeight, setUpperHomeHeight] = useState(0);
 
+  const [showText, setShowText] = useState(0);
+  const [secretPath, setSecretPath] = useState('/assets/media/skeleton.png');
 
   const upperHomeRef = useRef<HTMLDivElement>(null);
 
@@ -31,6 +33,16 @@ const Home: React.FC = () => {
       router.push('/posts');
     }, 1000)
   };
+
+  function clickSkele() {
+    if (showText == -1) return;
+    if (showText == 15) {
+      setShowText(-1);
+      setSecretPath('/assets/media/secret.gif')
+      return
+    }
+    setShowText(showText + 1);
+  }
 
   return (
     <>
@@ -64,14 +76,24 @@ const Home: React.FC = () => {
         >
           <Icon name='arrow-up-right-light.svg' alt='expand' width={20}></Icon>
         </div>
-        <div className='skeleton' style={{ position: 'absolute', bottom: '0', right: '0', overflow:'hidden', zIndex:'-1'}}>
+        <div onClick={clickSkele} className='skeleton' style={{  cursor: 'pointer', position: 'absolute', bottom: '0', right: '0', overflow:'hidden', zIndex:'-1'}}>
           <Image
-            style={{height:'auto', width:'370px', position: 'relative', bottom: '-11px', right: '-40px', transform: "rotateZ(-2.5deg)"}}
-            src="/assets/media/skeleton.png"
+            style={{height:'auto', width:'360px', position: 'relative', bottom: '-11px', right: '-40px', transform: "rotateZ(-2.5deg)"}}
+            src={secretPath}
             alt="skeleton"
             width="300"
             height="300"
           ></Image>
+          <div style={{ display: showText > 0 ? 'block':'none', backgroundColor:'azure', borderRadius: '5px', color:'darkslategrey', padding: '3px', textAlign: 'right', maxWidth: '11em', position: 'absolute', top: '60px', right: '80px', zIndex:'20', pointerEvents: 'none'}}>
+            {
+              showText == 1 ? 'hi, im jason' :
+              showText == 2 ? 'i need a favour, got it?' :
+              showText == 3 ? 'you can free my spirit from my corpse' :
+              showText == 4 ? 'just click my body a few times' :
+              showText == 5 ? "if you do, i'll give you something" :
+              showText == 7 ? "and i'll tell my spirit friends about you" : null
+            }
+          </div>
         </div>
 
         {/* <div style={{ position: 'relative', paddingRight: '20px' }}>
